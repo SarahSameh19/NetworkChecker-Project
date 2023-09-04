d79d84366cc23fc9280b9795d28a4e9f84830ebe
sarahsameh19 <sarahsameh31@gmail.com>
2023-09-04 20:42:37 +0300

    specialities-service

---
+++ src/app/speciality.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Speciality } from './spec';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SpecialityService {
+  private apiUrl = 'https://limitlesscareapi-development.azurewebsites.net/api/NetworkProviders/GetProviderFiltersCriteria?specialties=';
+
+  constructor(private http: HttpClient) {}
+
+  getSpecialities(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+}
